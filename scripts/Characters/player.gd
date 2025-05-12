5fd2abf3286c05ea0cd5ecc74b012fc45fd9c292
clefalto <hamomelett@gmail.com>
2025-05-12 01:05:19 -0700

    migrate to source-like movement system

--- scripts/Characters/player.gd
+++ scripts/Characters/player.gd
@@ -14,21 +14,29 @@ var current_state = INPUT_STATE.normal
 @export var MOUSE_SENS = 0.3
 
 # speed and walking
-var cur_speed = 5.0
+# OVERHAUL CONTROLLER
+var wish_dir := Vector3.ZERO
 @export_category("Movement")
-@export var MAX_WALKING_SPEED = 15.0
-@export var WALKING_SPEED = 50.0
-@export var MAX_AIR_SPEED = 30.0
-@export var CROUCH_SPEED = 3.0
-@export var JUMP_VELOCITY = 4.5
+# ground movement settings
+@export var walk_speed := 20.0
+@export var ground_accel := 14.0
+@export var ground_deccel := 10.0
+@export var ground_friction := 2.5
 
-@export var accel_speed = 10.0
-@export var ground_friction: float = 10.0
-@export var air_friction: float = 2.0
-
-var direction = Vector3.ZERO
+# air movement settings
+@export var air_cap := 0.5
+@export var air_accel := 800.0
+@export var air_move_speed := 500.0
+@export var air_deccel := 0.5
+@export var air_friction := 1.0
+@export var jump_velocity = 4.5
 
 var look_direction = Vector3.ZERO
+
+
+const HEADBOB_MOVE_AMOUNT = 0.01
+const HEADBOB_FREQUENCY = 1.0
+var headbob_time := 0.0
 
 # jumping
 var double_jumpable := false
@@ -43,19 +51,22 @@ var is_sliding: bool = false
 
 # Abilities
 @export_category("Abilities")
-@export var DASH_SPEED = 40
+# dashing
+@export var DASH_SPEED = 20
 @export var dash_accel = 5
-@export var DASH_COOLDOWN := 0.5
-var last_dash_time := -DASH_COOLDOWN
+@export var dash_cooldown := 0.5
+@export var dash_timer := 0.4 # dash lasts for this long
+var current_dash_time := 0.0 
+var last_dash_time := -dash_cooldown
 
 # h
 const height = 1.8
 
 # leaning
 @export_category("Camera Properties")
-@export var LEAN_MULT = 0.66
+@export var LEAN_MULT = 0.1
 @export var LEAN_SMOOTH = 10.0
-@export var LEAN_AMOUNT = 0.7 
+@export var LEAN_AMOUNT = 0.001
 var current_strafe_dir = 0
 
 # components
@@ -74,6 +85,9 @@ var current_health: int = max_health
 var can_take_damage: bool = true
 var is_healing: bool = false
 var healing_timer: float = 0.5
+
+func is_dashing() -> bool:
+	return current_dash_time > 0
 
 func _ready() -> void:
 	# setup health component
@@ -99,11 +113,19 @@ func _input(event: InputEvent) -> void:
 		if current_state != INPUT_STATE.dead:
 			rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS)) # yaw
 			head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENS)) # pitch
-			head.rotation.x = clamp(head.rotation.x, deg_to_rad(-75), deg_to_rad(80))
+			head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _headbob_effect(delta):
+	headbob_time += delta * self.velocity.length()
+	camera.position = Vector3(
+		cos(headbob_time * HEADBOB_FREQUENCY * 0.5) * HEADBOB_MOVE_AMOUNT,
+		sin(headbob_time * HEADBOB_FREQUENCY) * HEADBOB_MOVE_AMOUNT,
+		0.0
+	)
 
 # frame by frame
 func _process(delta: float):
-	lean_pivot.rotation.z = lerp(lean_pivot.rotation.z, current_strafe_dir * LEAN_MULT, delta * LEAN_SMOOTH) # this causes some weirdness when you look down/up, working on a fix
+	lean_pivot.rotation.z = lerp(lean_pivot.rotation.z, -current_strafe_dir * LEAN_MULT, delta * LEAN_SMOOTH) # this causes some weirdness when you look down/up, working on a fix
 	look_direction = -head.global_basis.z
 	
 	if is_healing == true:
@@ -119,135 +141,94 @@ func _process(delta: float):
 	if Input.is_action_just_released("slide"):
 		is_sliding = false
 
+
+func _handle_air_physics(delta: float) -> void:
+	if is_dashing(): return
+	
+	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	var capped_speed = min((air_move_speed * wish_dir).length(), air_cap)
+	var add_speed_til_cap = capped_speed - (cur_speed_in_wish_dir)
+	if add_speed_til_cap > 0:
+		var accel_speed = air_accel * air_move_speed * delta
+		accel_speed = min(accel_speed, add_speed_til_cap)
+		self.velocity += accel_speed * wish_dir
+
+
+func _handle_ground_physics(delta: float) -> void:
+	if is_dashing(): return
+	
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	var add_speed_til_cap = walk_speed - cur_speed_in_wish_dir
+	if add_speed_til_cap > 0:
+		var accel_speed = ground_accel * delta * walk_speed
+		accel_speed = min(accel_speed, add_speed_til_cap)
+		self.velocity += accel_speed * wish_dir
+	
+	# friction
+	var control = max(self.velocity.length(), ground_deccel)
+	#print("velocity length: %s" % self.velocity.length())
+	var drop = control * ground_friction * delta * 0.05
+	#print("drop: %s" % drop)
+	var new_speed = max(self.velocity.length() - drop, 0.0)
+	if self.velocity.length() > 0:
+		new_speed /= self.velocity.length()
+	self.velocity *= new_speed
+	
+	_headbob_effect(delta)
+
 # frame by frame physics
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	else: 
-		double_jumpable = true # when you touch the ground you can double jump again
-	
-	
-	
-	if current_state == INPUT_STATE.normal:
-		var h_rot = global_transform.basis.get_euler().y
-		if self.position.y < DEATH_HEIGHT:
-			kill_player()
-			
-		cur_speed = WALKING_SPEED
 		
-		# jumping
-		handle_jumping()
-		
-		var input_dir = Input.get_vector("left", "right", "forward", "back")
-		if (input_dir.x < 0):
-			current_strafe_dir = LEAN_AMOUNT
-		elif (input_dir.x > 0):
-			current_strafe_dir = -LEAN_AMOUNT
-		else:
-			current_strafe_dir = 0
-		
-		
-		
-		#direction = lerp(direction, (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
-		direction = lerp(direction, Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, h_rot).normalized(), delta * accel_speed)
-		
-		#direction = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, h_rot).normalized()
-		
-		# moving somewhere
-		if direction:
-			#velocity.x += direction.x * cur_speed * delta
-			#velocity.z += direction.z * cur_speed * delta
-			#
-			## decceleration
-			var fric = Vector3.ZERO
-			if is_on_floor():
-				fric = ground_friction
-			else: 
-				fric = air_friction
-			#
-			##print(Vector3(velocity.x, 0.0, velocity.z).length())
-			#var max_speed = Vector3.ZERO
+		## transition to sliding state
+		#if is_sliding: 
+			#current_state = INPUT_STATE.sliding
+			#head.position.y = slide_height
+			## give initial boost of speed if we're grounded
 			#if is_on_floor():
-				#max_speed = MAX_WALKING_SPEED
-			#else:
-				#max_speed = MAX_AIR_SPEED
-			#
-			#if Vector3(velocity.x, 0.0, velocity.z).length() >= max_speed:
-				#var vectorized_max_speed = Vector3(velocity.x, 0.0, velocity.z).normalized() * max_speed
-				#velocity.x = move_toward(velocity.x, vectorized_max_speed.x, fric * delta)
-				#velocity.z = move_toward(velocity.z, vectorized_max_speed.z, fric * delta)
-			
-			
-			# X check if we're above walking speed (dashing)
-			if Vector3(velocity.x, 0.0, velocity.z).length() <= WALKING_SPEED:
-				velocity.x = direction.x * cur_speed
-				velocity.z = direction.z * cur_speed
-			else:
-				var vectorized_max_speed = Vector3(velocity.x, 0.0, velocity.z).normalized() * WALKING_SPEED
-				velocity.x = move_toward(velocity.x, vectorized_max_speed.x, fric * delta)
-				velocity.z = move_toward(velocity.z, vectorized_max_speed.z, fric * delta)
-				#velocity.x = move_toward(velocity.x, sign(velocity.x) * WALKING_SPEED, fric)
-			#elif velocity.x > 0:
-				#velocity.x -= dash_accel
-			#elif velocity.x < 0:
-				#velocity.x += dash_accel
-			
-			## Z check if we're above walking speed (dashing)
-			#if abs(velocity.z) <= WALKING_SPEED:
-				#velocity.z = direction.z * cur_speed
-			#else:
-				#velocity.z = move_toward(velocity.z, sign(velocity.z) * WALKING_SPEED, fric)
-			##elif velocity.z > 0:
-				##velocity.z -= dash_accel
-			##elif velocity.z < 0:
-				##velocity.z += dash_accel
-		
-		else:
-			var fric = Vector2.ZERO
-			if is_on_floor():
-				fric = ground_friction
-			else: 
-				fric = air_friction
-			velocity.x = move_toward(velocity.x, 0.0, fric * delta)
-			velocity.z = move_toward(velocity.z, 0.0, fric * delta)
-			#head.rotate_object_local()
-		
-		# transition to sliding state
-		if is_sliding: 
-			current_state = INPUT_STATE.sliding
-			head.position.y = slide_height
-			# give initial boost of speed if we're grounded
-			if is_on_floor():
-				var add_amount = velocity.normalized() * slide_speed_boost
-				velocity += add_amount
-		
-		# shooting
-		handle_shooting()
-		
-		
-	if current_state == INPUT_STATE.sliding:
-		# keep velocity we entered with, slowly decrease it
-		velocity.x = move_toward(velocity.x, 0, slide_deccel*delta)
-		velocity.z = move_toward(velocity.z, 0, slide_deccel*delta)
-		
-		handle_jumping()
-		handle_shooting()
-		
-		if !is_sliding:
-			current_state = INPUT_STATE.normal
-			head.position.y = normal_height
+				#var add_amount = velocity.normalized() * slide_speed_boost
+				#velocity += add_amount
+		#
+		## shooting
+		#handle_shooting()
+		#
+		#
+	
+	var input_dir = Input.get_vector("left", "right", "forward", "back").normalized()
+	
+	current_strafe_dir = input_dir.x
+	
+	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)
+	
+	handle_jumping()
+	handle_shooting()
+	if is_dashing():
+		current_dash_time -= delta
+		if current_dash_time <= 0.0:
+			handle_finish_dash()
+	
+	if is_on_floor():
+		_handle_ground_physics(delta)
+		double_jumpable = true
+	else:
+		_handle_air_physics(delta)
+	
+	if self.position.y < DEATH_HEIGHT:
+		kill_player()
 	
 	move_and_slide()
 
 func handle_jumping():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if is_dashing:
+			current_dash_time = 0.0 # stop dash
+		self.velocity.y = jump_velocity
 		is_sliding = false
 	elif Input.is_action_just_pressed("jump") and not is_on_floor() and double_jumpable:
 		# double jumping
 		double_jumpable = false
-		velocity.y = JUMP_VELOCITY
+		self.velocity.y = jump_velocity
 		is_sliding = false
 
 func handle_shooting():
@@ -256,43 +237,25 @@ func handle_shooting():
 	if Input.is_action_just_released("shoot"):
 		weapon.cease_fire()
 
-# health specific functionality
-#func take_damage(amount: int) -> void:
-	#if current_state == INPUT_STATE.dead:
-		#return
-		#
-	#current_health -= amount
-	#health_change.emit()
-	#print("Player took", amount, "damage. Health:", current_health)
-	#
-	#if current_health <= 0:
-		#die()
-
-# you dead
-#func die():
-	## omae wa mou shindeiru
-	#if current_state == INPUT_STATE.dead:
-		#return
-		#
-	## invoke player_die signal
-	#current_state = INPUT_STATE.dead
-	#emit_signal("player_die")
-	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	#print("Player has died.")
+func handle_finish_dash() -> void:
+	self.velocity /= 5
 
 # recieve dash input from WeaponManager
 func _on_weapon_manager_dash_input() -> void:
-	if current_state != INPUT_STATE.normal:
+	if current_state != INPUT_STATE.normal or is_dashing():
 		return
 	
 	var current_time := Time.get_ticks_msec() / 1000.0 # get time in seconds
-	if current_time - last_dash_time < DASH_COOLDOWN:
+	if current_time - last_dash_time < dash_cooldown:
 		return # skip dash
 	
 	last_dash_time = current_time # timer reset
-	#var dash_vel: Vector3 = direction.normalized()
-	var dash_vel: Vector3 = look_direction.normalized()
+	var dash_vel: Vector3 = wish_dir.normalized()
+	#var dash_vel: Vector3 = look_direction.normalized()
 	print("dashing! input: " + str(dash_vel))
+	
+	current_dash_time = dash_timer
+	
 	velocity += dash_vel * DASH_SPEED * Vector3(1, 0.2, 1)
 	#velocity.y = 0
 	#print("velocity: " + str(velocity))
