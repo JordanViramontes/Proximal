64cf59a5186820aa576600980811883fb61963e4
teejay-nguyen <tommynguyen778@gmail.com>
2025-05-14 06:54:30 -0700

    Fixed parallax motion script and added middle layer of menu

--- scenes/UI/ParallaxMotion.gd
+++ scenes/UI/ParallaxMotion.gd
@@ -3,14 +3,14 @@ extends Sprite2D
 @export var move_force : int = 1
 @export var amplitude: float = 4.0
 @export var speed: float = 0.5
+@export var lag_point: float = 0
 
-var start_position: Vector2
 var time_passed: float = 0.0
-
-func _ready() -> void:
-	start_position = position
 
 func _physics_process(delta: float) -> void:
 	time_passed += delta
+	# parallax effect - moves UI element based on global mouse position
 	position += (get_global_mouse_position() / move_force * delta) - position
-	position.y = position.y + sin(time_passed * speed) * amplitude
+	
+	# bobbing
+	position.y = position.y + sin((time_passed * speed) - deg_to_rad(lag_point)) * amplitude
