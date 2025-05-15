fc3c0434340fb93fb2365f35c16bcdbb737c38ae
teejay-nguyen <tommynguyen778@gmail.com>
2025-05-14 17:42:45 -0700

    Added options menu with volume slider and resolution change

--- menu.gd
+++ menu.gd
@@ -8,7 +8,32 @@ func _on_play_pressed():
 
 # options button should allow to change ui elements, key binds, etc.
 func _on_options_pressed():
-	pass
+	$"Options Menu".visible = true
 	
 func _on_quit_pressed():
 	get_tree().quit()
+
+# audio slider function
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+	print("volume set to: " + str(value))
+
+# resolution setting
+func _on_resolutions_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+		3:
+			DisplayServer.window_set_size(Vector2i(1024, 576))
+		4:
+			DisplayServer.window_set_size(Vector2i(640, 360))
+		5:
+			DisplayServer.window_set_size(Vector2i(256, 144))
+
+
+func _on_options_exit_pressed() -> void:
+	$"Options Menu".visible = false
