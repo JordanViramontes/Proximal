eb1f87e0cdaeeec9f6c77526d8a5102f3b425cdc
JordanViramontes <jvira007@ucr.edu>
2025-06-04 13:52:12 -0700

    added main music

--- Main/Areas/world.gd
+++ Main/Areas/world.gd
@@ -4,6 +4,18 @@ static var world
 var timer_off: bool = false
 var reset_timer: Timer
 
+#region audio
+# sound effects
+@onready var audio_manager: Node3D = $AudioManager
+signal sound_effect_signal_start(name: String)
+signal sound_effect_signal_stop(name: String)
+
+var MUS_ambiance: String = "ambiance"
+@onready var music: Dictionary[String, AudioStreamPlayer] = {
+	MUS_ambiance:$AudioManager/AudioStreamPlayer,
+}
+#endregion
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	world = self
@@ -13,15 +25,19 @@ func _ready() -> void:
 	
 	# volume level
 	AudioServer.set_bus_volume_db(0, 1.0)
+	
+	# sound effects
+	for i in music.keys():
+		audio_manager.sound_effects[i] = music[i]
+	self.sound_effect_signal_start.connect(audio_manager.play_sfx)
+	self.sound_effect_signal_stop.connect(audio_manager.stop_sfx)
+	
+	sound_effect_signal_start.emit(MUS_ambiance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("hotkey_index"):
-		print("playing")
-		var audio = AudioStreamPlayer.new()
-		audio.stream = load("res://assets/Sounds/Sound Effects/Player/jumping.wav")
-		add_child(audio)
-		audio.play()
+	if not music[MUS_ambiance].is_playing():
+		sound_effect_signal_start.emit(MUS_ambiance)
 	
 	if reset_timer:
 		if timer_off:
